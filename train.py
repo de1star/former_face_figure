be440ccb8430fff3bf74860db9e8322264341fd9
Xinchi <843414674@qq.com>
2022-03-17 08:57:26 -0400

    update params

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-import torch, os, pickle
+import torch, os, pickle, datetime
 from torch.optim import AdamW
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from utils.custom_model import MyModel, MyConfig
@@ -12,25 +12,25 @@ def main():
 
 
 def test():
+    time = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    model_output = 'mymodel'
+    if not os.path.exists(model_output):
+        os.makedirs(model_output)
     dataset = Dataset_F3('train')
     dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
     config = MyConfig()
     model = MyModel(config).cuda()
-    epoch = 3
+    epoch = 8
     # initialize the optimizer, I used AdamW here.
     optimizer = AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.98))
     # learning rate scheduler, I did not warm up the model.
-    scheduler = CosineAnnealingLR(optimizer, T_max=3, eta_min=1e-5)
+    scheduler = CosineAnnealingLR(optimizer, T_max=7, eta_min=1e-5)
     loss_func = torch.nn.MSELoss()
-    max_len = 5000
     accumulation_steps = 4
     for e in range(epoch):
         for _, batch in tqdm(enumerate(dataloader)):
             p1_vectors = batch['input'].to(torch.float32).cuda()
             p2_vectors = batch['output'].to(torch.float32).cuda()
-            if p1_vectors.shape[1] > max_len:
-                p1_vectors = p1_vectors[:, :max_len, :]
-                p2_vectors = p2_vectors[:, :max_len, :]
             output = model(p1_vectors=p1_vectors, p2_vectors=p2_vectors)
             loss = loss_func(output, p2_vectors)
             loss = loss / accumulation_steps
@@ -41,6 +41,7 @@ def test():
                 print(loss)
         optimizer.step()
         optimizer.zero_grad()
+        torch.save(model.state_dict(), os.path.join(model_output, f"f3_model_{time}"))
 
 
 if __name__ == '__main__':
