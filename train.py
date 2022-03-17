08ffc96be38616e937873593c17b7574d5c80e37
Xinchi <843414674@qq.com>
2022-03-17 08:39:29 -0400

    update model

--- train.py
+++ train.py
@@ -1,23 +1,47 @@
 import torch, os, pickle
-from transformers import LongformerTokenizer, LongformerConfig, LongformerForQuestionAnswering
+from torch.optim import AdamW
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from utils.custom_model import MyModel, MyConfig
 from torch.utils.data import DataLoader
 from utils.dataset import Dataset_F3
+from tqdm import tqdm
 
 
 def main():
-    config = LongformerConfig()
-    model = LongformerForQuestionAnswering(config)
-    dataset = Dataset_F3('valid')
+    pass
+
+
+def test():
+    dataset = Dataset_F3('train')
     dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
-    epoch = 1
+    config = MyConfig()
+    model = MyModel(config).cuda()
+    epoch = 3
+    # initialize the optimizer, I used AdamW here.
+    optimizer = AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.98))
+    # learning rate scheduler, I did not warm up the model.
+    scheduler = CosineAnnealingLR(optimizer, T_max=3, eta_min=1e-5)
+    loss_func = torch.nn.MSELoss()
+    max_len = 5000
+    accumulation_steps = 4
     for e in range(epoch):
-        for _, batch in enumerate(dataloader):
-            input = batch['input'].squeeze(0)[5:]
-            true_output = batch['output'].squeeze(0)[5:]
-            attention_mask = torch.ones(input.shape[0])
-            output = model(attention_mask=attention_mask, inputs_embeds=input)
-            print(output)
+        for _, batch in tqdm(enumerate(dataloader)):
+            p1_vectors = batch['input'].to(torch.float32).cuda()
+            p2_vectors = batch['output'].to(torch.float32).cuda()
+            if p1_vectors.shape[1] > max_len:
+                p1_vectors = p1_vectors[:, :max_len, :]
+                p2_vectors = p2_vectors[:, :max_len, :]
+            output = model(p1_vectors=p1_vectors, p2_vectors=p2_vectors)
+            loss = loss_func(output, p2_vectors)
+            loss = loss / accumulation_steps
+            loss.backward()
+            if (_ + 1) % accumulation_steps == 0:
+                optimizer.step()
+                optimizer.zero_grad()
+                print(loss)
+        optimizer.step()
+        optimizer.zero_grad()
 
 
 if __name__ == '__main__':
-    main()
+    test()
