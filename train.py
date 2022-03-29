147c6604e0b0f2f84b2ee07618a44b97b482622f
Xinchi <843414674@qq.com>
2022-03-29 05:40:14 -0400

    modifications on loss calculations

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ def test():
     valid_dataloader = DataLoader(valid_dataset, batch_size=1)
     config = MyConfig()
     model = MyModel(config).cuda()
-    epoch = 100
+    epoch = 200
     # initialize the optimizer, I used AdamW here.
     optimizer = AdamW(model.parameters(), lr=2e-6, betas=(0.9, 0.98))
     # learning rate scheduler, I did not warm up the model.
